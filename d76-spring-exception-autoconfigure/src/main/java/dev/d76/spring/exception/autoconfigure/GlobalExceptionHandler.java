ced1f991e92233f194f11b2d8bd7010578921893
dhanesh76 <dhaneshvenkateslu@gmail.com>
2026-04-19 12:39:42 +0530

    chore: bump version to 2.0.0

--- d76-spring-exception-autoconfigure/src/main/java/dev/d76/spring/exception/autoconfigure/GlobalExceptionHandler.java
+++ d76-spring-exception-autoconfigure/src/main/java/dev/d76/spring/exception/autoconfigure/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import dev.d76.spring.exception.ErrorCode;
 import dev.d76.spring.exception.autoconfigure.web.ApiErrorResponse;
 import dev.d76.spring.exception.autoconfigure.web.ApiErrorResponseCustomizer;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
@@ -27,20 +28,25 @@ public final class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(BusinessException.class)
-    ResponseEntity<ApiErrorResponse> handleBusinessException(BusinessException ex,
-                                                             HttpServletRequest request) {
+    ResponseEntity<ApiErrorResponse> handleBusinessException(
+            BusinessException ex,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
 
         ErrorCode errorCode = ex.getErrorCode();
         var responseBuilder = ApiErrorResponse.builderFrom(errorCode, ex.getMessage(), request);
 
-        applyCustomizers(responseBuilder, ex, request);
+        applyCustomizers(responseBuilder, ex, request, response);
 
         return ResponseEntity.status(errorCode.getHttpStatus()).body(responseBuilder.build());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity<ApiErrorResponse> handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException ex, HttpServletRequest request
+            MethodArgumentNotValidException ex,
+            HttpServletRequest request,
+            HttpServletResponse response
     ) {
         List<ApiErrorResponse.ApiFieldError> fieldErrors = ex
                 .getBindingResult().getFieldErrors()
@@ -58,26 +64,29 @@ public final class GlobalExceptionHandler {
                 .builderFrom(errorCode, request)
                 .errors(fieldErrors);
 
-        applyCustomizers(responseBuilder, ex, request);
+        applyCustomizers(responseBuilder, ex, request, response);
 
         return ResponseEntity.status(errorCode.getHttpStatus()).body(responseBuilder.build());
     }
 
     @ExceptionHandler(Exception.class)
-    ResponseEntity<ApiErrorResponse> handleException(HttpServletRequest request, Exception ex) {
+    ResponseEntity<ApiErrorResponse> handleException(
+            Exception ex,
+            HttpServletRequest request,
+            HttpServletResponse response) {
 
         var errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
 
         var responseBuilder = ApiErrorResponse.builderFrom(errorCode, request);
 
-        applyCustomizers(responseBuilder, ex, request);
+        applyCustomizers(responseBuilder, ex, request, response);
 
         return ResponseEntity.status(errorCode.getHttpStatus()).body(responseBuilder.build());
     }
 
-    private void applyCustomizers(ApiErrorResponse.Builder builder, Exception ex, HttpServletRequest request) {
+    private void applyCustomizers(ApiErrorResponse.Builder builder, Exception ex, HttpServletRequest request, HttpServletResponse response) {
         for (var customizer : customizers) {
-            customizer.customize(builder, ex, request);
+            customizer.customize(builder, ex, request, response);
         }
     }
 }
--- d76-spring-exception-autoconfigure/src/main/java/dev/d76/spring/exception/autoconfigure/web/ApiErrorResponseCustomizer.java
+++ d76-spring-exception-autoconfigure/src/main/java/dev/d76/spring/exception/autoconfigure/web/ApiErrorResponseCustomizer.java
@@ -2,8 +2,13 @@ package dev.d76.spring.exception.autoconfigure.web;
 
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @FunctionalInterface
 public interface ApiErrorResponseCustomizer {
-    void customize(ApiErrorResponse.Builder builder, Exception ex, HttpServletRequest request);
+    void customize(ApiErrorResponse.Builder builder,
+                   Exception ex,
+                   HttpServletRequest request,
+                   HttpServletResponse response
+    );
 }
